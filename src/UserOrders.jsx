9dba3c431dbc9eec6210b6b4245f693a9178c30b
Your Name <you@example.com>
2022-06-19 19:55:43 +0530

    orders

--- src/UserOrders.jsx
+++ src/UserOrders.jsx
@@ -24,7 +24,7 @@ function OrderList(props) {
   useEffect(() =>{
     (
     async () =>{
-      const response =  await fetch('https://pharmacymanagementwebapiservice20220617105735.azurewebsites.net/api/User',{
+      const response =  await fetch('https://pharmacymanagementwebapiservice20220617105735.azurewebsites.net/api/Auth/User',{
                 method:'GET',
                      headers:{'Content-Type':'application/json'},
                     credentials:'include',
